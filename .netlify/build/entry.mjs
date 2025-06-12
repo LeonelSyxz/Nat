a1fa1bb44bf1f75c4415a0d0edcf81e43047b6bf
Edgar Leonel Acevedo Cuevas <leonelsyxz@gmail.com>
2025-06-11 23:21:29 -0600

    Deploy Three

--- .netlify/build/entry.mjs
+++ .netlify/build/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
-import { manifest } from './manifest_CdCQxJ-l.mjs';
+import { manifest } from './manifest_BgDROOpd.mjs';
 import { createExports } from '@astrojs/netlify/ssr-function.js';
 
 const serverIslandMap = new Map();;
@@ -18,7 +18,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "5e77517b-851a-4982-98c4-ae3431bc47b0"
+    "middlewareSecret": "e79140e3-b075-4624-9291-5c7fa5b03a51"
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
